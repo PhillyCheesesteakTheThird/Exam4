3b120b1f2e16f7a80352cf9914dbf78aacdff383
Jadan Kerr <badi1863@gmail.com>
2023-12-10 17:23:33 -0700

    Did some things

--- src/DiceGame.java
+++ src/DiceGame.java
@@ -1,8 +1,14 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DiceGame {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File output = new File("DiceGameOutput.txt");
+        FileWriter fileWriter = new FileWriter(output);
+        System.out.println(output.getPath());
         Scanner scanner = new Scanner(System.in);
         ArrayList<Player> playerList = new ArrayList<>();
 
@@ -20,11 +26,13 @@ public class DiceGame {
         }
         for (Player player : playerList) {
             player.getDie().roll();
+            fileWriter.write(player.getName() + " rolled a " + player.getDie().getValue() + " on a " + player.getDie().getNumSides() + " sided Die." + "\n");
             System.out.println(player.getName() + " rolled a " + player.getDie().getValue() + " on a " + player.getDie().getNumSides() + " sided Die.");
         }
-        System.out.println(decideWinner(playerList));
-
-
+        String winner = decideWinner(playerList);
+        fileWriter.write(winner);
+        System.out.println(winner);
+        fileWriter.close();
     }
 
     public static String decideWinner(ArrayList<Player> playerList) {
@@ -34,7 +42,7 @@ public class DiceGame {
             if (playerList.get(0).getDie().getValue() > playerList.get(1).getDie().getValue()) {
                 playerList.remove(1);
             } else if (playerList.get(0).getDie().getValue() == playerList.get(1).getDie().getValue()) {
-                return playerList.get(0).getName() + playerList.get(1).getName() + " tied the game.";
+                return playerList.get(0).getName() + " and " + playerList.get(1).getName() + " tied the game.";
             } else {
                 playerList.remove(0);
             }
